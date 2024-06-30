38ccc64f187099b551a289b7eeebe61fb4b61da7
Tanisha <tanisha.engineer2005@gmail.com>
2024-06-30 14:18:04 +0530

    Hierarchial Inheriatance

--- OOPs/Inheritance.java
+++ OOPs/Inheritance.java
@@ -6,10 +6,17 @@ public class Inheritance {
         // Fish shark = new Fish();
         // shark.eat();
 
-        Dog dobby = new Dog();
-        dobby.eat();
-        dobby.legs = 4;
-        System.out.println(dobby.legs);
+        // Dog dobby = new Dog();
+        // dobby.eat();
+        // dobby.legs = 4;
+        // System.out.println(dobby.legs);
+
+        Bird bird = new Bird();
+        bird.fly();
+        bird.eat();
+        Fish fist = new Fish();
+        fist.swim();
+        fist.breathe();
 
     }
 }
@@ -26,14 +33,32 @@ class Animal{
 
 }
 
-// Multi level Inheritance (Base class -> subclass1 -> subclass2)
+// Hierarchial Inheritance (class A -> class B, class A -> class C, class A -> class D)
 class Mammals extends Animal{
-    int legs;
+    void walk(){
+        System.out.println("walks");
+    }
+}
+class Fish extends Animal{
+    void swim(){
+        System.out.println("swims");
+    }
+}
+class Bird extends Animal{
+    void fly(){
+        System.out.println("flies");
+    }
 }
 
-class Dog extends Mammals{
-    String breed;
-}
+
+// Multi level Inheritance (Base class -> subclass1 -> subclass2)
+// class Mammals extends Animal{
+//     int legs;
+// }
+
+// class Dog extends Mammals{
+//     String breed;
+// }
 
 
 // Single Level Inheritance (One base class -> one derived class)
