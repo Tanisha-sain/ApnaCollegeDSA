352f932264bd75a0560694224185c2a69f7f07b8
Tanisha <tanisha.engineer2005@gmail.com>
2024-07-01 21:39:40 +0530

    Question related to OOps

---
+++ PracticeSheets/OOPs_11/ElectricCar.java
@@ -0,0 +1,24 @@
+package PracticeSheets.OOPs_11;
+
+class Automobile{
+    private String drive(){
+        return "Driving vehicle";
+    }
+}
+
+class Car extends Automobile{
+    protected String drive(){
+        return "Driving car";
+    }
+}
+
+public class ElectricCar extends Car{
+    @Override
+    public final String drive(){
+        return "Driving electric car";
+    }
+    public static void main(String[] args) {
+        final Car car = new ElectricCar();
+        System.out.println(car.drive());
+    }
+}
