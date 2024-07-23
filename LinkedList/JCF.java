762e23b6acbb57c5729a17b7f66c7a1b4df6c09a
Tanisha <tanisha.engineer2005@gmail.com>
2024-07-23 15:32:25 +0530

    java collection framework

---
+++ LinkedList/JCF.java
@@ -0,0 +1,17 @@
+package LinkedList;
+import java.util.LinkedList;
+public class JCF {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addLast(1);
+        list.addLast(2);
+        list.addFirst(8);
+        list.addFirst(7);
+        System.out.println(list);
+
+        list.removeLast();
+        list.removeFirst();
+        System.out.println(list);
+    }
+}
