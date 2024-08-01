09e9d6f445ec6d20824b13abbc697e704b9e9297
Tanisha <tanisha.engineer2005@gmail.com>
2024-08-01 12:59:27 +0530

    Deletion of nodes in bst

--- Tree/BST.java
+++ Tree/BST.java
@@ -24,12 +24,41 @@ public class BST {
         if(root.data < key) return search(root.right, key);
         else return search(root.left, key);
     }
+
+    static Node delete(Node root, int val){
+        if(root.data < val){
+            root.right = delete(root.right, val);
+        }else if(root.data > val){
+            root.left = delete(root.left, val);
+        }else{
+            if(root.left == null && root.right == null){
+                return null;
+            }else if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }else{
+                Node inSuc = inorderSuccessor(root.right);
+                root.data = inSuc.data;
+                root.right = delete(root.right, inSuc.data);
+            }
+        }
+        return root;
+    }
+
+    static Node inorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
     public static void main(String[] args) {
         int[] arr = {5,1,3,4,2,7};
         Node root = buildBST(arr);
 
+        root = delete(root, 3);
         btree.inOrder(root);
         System.out.println();
-        System.out.println(search(root, 8));
+        System.out.println(search(root, 3));
     }
 }
